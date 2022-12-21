2d173bceab62472969e9cab30f0d02348712b197
MiroslavKosaric <114644551+MiroslavKosaric@users.noreply.github.com>
2022-12-21 11:29:27 +0100

    Add files via upload

--- lang/indexEN.php
+++ lang/indexEN.php
@@ -57,50 +57,53 @@
     <link rel="stylesheet" type="text/css" media='screen and (max-width: 600px)' href="../css/mobile.css">
     <link rel="stylesheet" type="text/css" media='screen and (min-width: 601px)' href="../css/style.css">
     <script type="text/javascript" src="../js/main.js"></script>
-
+    
     <title>SmartHome Control Centre</title>
 </head>
 <body onload="setIndicatorColors()">
-<header>
-    <h1>KGBSmartHome Control Centre</h1>
-    <span>
-        <ul class="languages">
-            <li><a href="../index.php"><img src="../images/sk_flag.png" id="sk" class="lang"></a></li>
-            <li><a href="indexEN.php"><img src="../images/en_flag.png" id="en" class="lang"></a></li>
-        </ul>
-    </span>
-</header>
+    <header>
+        <h1>KGBSmartHome Control Centre</h1>
+        <div class="languages">
+            <a href="../index.html"><img src="../images/sk_flag.png" id="sk" class="lang"></a>
+            <a href="indexEN.html"><img src="../images/en_flag.png" id="en" class="lang"></a>
+        </div>
+    </header>
 
-<main>
-    <center>
-        <section class="controlPanelContainer">
-            <h2 class="heading">Control panel</h2>
-            <form class="controlPanel" action="" method="POST">
-                <label for="blinds">Blinds</label>
-                <p id="blindsIndicator" class="indicator blindsIndicator">&#9679;</p>
-                <div class="slider">
-                    <input id="slider" name="blinds" type="range" min="0" max="100" value="50" oninput="changeRangeIndicatorColor()">
-                </div>
+    <main>
+        <center>
+            <section class="controlPanelContainer">
+                <h2 class="heading">Control panel</h2>
+                <form class="controlPanel" action="" method="POST">
+                    <label for="blinds">Blinds</label>
+                    <p id="blindsIndicator" class="indicator blindsIndicator">&#9679;</p>
+                    <div class="slider">
+                        <input id="slider" name="blinds" type="range" min="0" max="100" value="50" oninput="changeRangeIndicatorColor()">
+                    </div>
 
-                <br><br>
-                <label for="fan">Fan</label>
-                <p class="indicator fanIndicator">&#9679;</p>
-                <input type="button" name="fan" class="toggleFan" onclick="changeIndicatorColor('fanIndicator')" value="Toggle">
+                    <br>
+                    <label for="fan">Fan</label>
+                    <p class="indicator fanIndicator">&#9679;</p>
+                    <input type="button" name="fan" class="toggleFan" onclick="changeIndicatorColor('fanIndicator')" value="Toggle">
+                    <!-- <span class="required">* <?php echo $numError;?></span> -->
 
-                <br><br>
-                <label for="outdoorLight">Outdoor light</label>
-                <p class="indicator outdoorLightIndicator">&#9679;</p>
-                <input type="button" name="outdoorLight" class="toggleOutdoorLight" onclick="changeIndicatorColor('outdoorLightIndicator')" value="Toggle">
-            </form>
-        </section>
-    </center>
-</main>
+                    <br><br>
+                    <label for="outdoorLight">Outdoor light</label>
+                    <p class="indicator outdoorLightIndicator">&#9679;</p>
+                    <input type="button" name="outdoorLight" class="toggleOutdoorLight" onclick="changeIndicatorColor('outdoorLightIndicator')" value="Toggle"> 
+                    <br><br>
+                </form>
+                <center>
+                <input type="submit" value="Uložiť zmeny"/>
+                </center>
+            </section>
+        </center>
+    </main>
 
-<footer>
-    <center>
-        <p>Made with ❤️ by KGB company.</p>
-        <p>&#169;2022</p>
-    </center>
-</footer>
+    <footer>
+        <center>
+            <p>Made with ❤️ by KGB company.</p>
+            <p>&#169;2022</p>
+        </center>
+    </footer>
 </body>
 </html>
